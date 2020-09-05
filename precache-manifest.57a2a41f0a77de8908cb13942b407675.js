425be889c86be9b5bc662a2e520fd750eb14fa55
Abhilash Gnan <abhilashgnan@gmail.com>
2020-09-05 21:14:28 +0200

    Updates

---
+++ precache-manifest.57a2a41f0a77de8908cb13942b407675.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da17dc53de877783ef58c8528305c566",
+    "url": "/sudoku/index.html"
+  },
+  {
+    "revision": "5f596d9a7fd7a29007ed",
+    "url": "/sudoku/static/css/2.5b44f0a0.chunk.css"
+  },
+  {
+    "revision": "1136e8b75014af29d2b4",
+    "url": "/sudoku/static/css/main.b97998ca.chunk.css"
+  },
+  {
+    "revision": "5f596d9a7fd7a29007ed",
+    "url": "/sudoku/static/js/2.e9255229.chunk.js"
+  },
+  {
+    "revision": "c836aaadfe43dfd9fcb8ddeddfd61e18",
+    "url": "/sudoku/static/js/2.e9255229.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1136e8b75014af29d2b4",
+    "url": "/sudoku/static/js/main.7bfc2195.chunk.js"
+  },
+  {
+    "revision": "8d8e892f47ac5d31712b",
+    "url": "/sudoku/static/js/runtime-main.f7976529.js"
+  }
+]);
